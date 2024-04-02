e9fece900b9112a4571963d34bfb68e3eb5c4dba
+ <+>
2024-04-02 22:38:38 +0300

    fetch-lab

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 function fetchBooks() {
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => console.log(json));
+  }
+  then((json) => console.log(json))
   // To pass the tests, don't forget to return your fetch!
   
-}
-
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const main = document.getElementbyId("book-list");
+
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+    const listItem = document.createElement('Li');
+    listItem.testContent = book.name;
+    main.appendChild(listItem);
   });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+// Call fetchbook() when the page is loaded document.addEventListener("DOMContentLoaded", fetchbooks); 
