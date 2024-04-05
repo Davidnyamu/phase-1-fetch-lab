989ebf3d25c7b2cd6eaacf0f240de3b40d66806b
+ <+>
2024-04-05 15:39:37 +0300

    fetch-lab

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 function fetchBooks() {
-  fetch("https://anapioficeandfire.com/api/books")
-  .then((resp) => resp.json())
-  .then((json) => console.log(json));
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then ((resp) => resp.json())
+  .then ((jsonData) => renderBooks(jsonData))
   }
-  then((json) => console.log(json))
-  // To pass the tests, don't forget to return your fetch!
-  
-function renderBooks(books) {
-  const main = document.getElementbyId("book-list");
 
+function renderBooks(books) {
+  const main = document.querySelector('main');
   books.forEach(book => {
-    const listItem = document.createElement('Li');
-    listItem.testContent = book.name;
-    main.appendChild(listItem);
+    const h2 = document.createElement('h2');
+    h2.innerHTML = book.name;
+    main.appendChild(h2);
   });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
-
-// Call fetchbook() when the page is loaded document.addEventListener("DOMContentLoaded", fetchbooks); 
